21e145b9144d64d0754dde3b7fe6de63c4387b43
dominikwantuch <dominikwantuch@protonmail.com>
2019-10-11 11:06:48 +0200

    Add FakeProductRepository.

---
+++ SportShop/Models/FakeProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+// ReSharper disable All
+
+namespace SportShop.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product>
+        {
+            new Product() {Name = "Piłka nożna", Price = 25},
+            new Product() {Name = "Deska surfingowa", Price = 179},
+            new Product() {Name = "Buty do biegania", Price = 95}
+        }.AsQueryable<Product>();
+    }
+}

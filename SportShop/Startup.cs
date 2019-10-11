559f17e2bff89f469d78ff6133780cd04223e3dd
dominikwantuch <dominikwantuch@protonmail.com>
2019-10-11 10:44:01 +0200

    Configure basic functionality.

--- SportShop/Startup.cs
+++ SportShop/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using SportShop.Models;
 
 namespace SportShop
 {
@@ -15,6 +16,8 @@ namespace SportShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IProductRepository, FakeProductRepository>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,7 +28,9 @@ namespace SportShop
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) => { await context.Response.WriteAsync("Hello World!"); });
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvc();
         }
     }
 }

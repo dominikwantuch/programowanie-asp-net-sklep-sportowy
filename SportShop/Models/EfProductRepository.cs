58b43e8d5e5e25aa6bff93d66fba24af7bfdafae
dominikwantuch <dominikwantuch@protonmail.com>
2019-10-11 11:34:46 +0200

    Add EF repository.

---
+++ SportShop/Models/EfProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace SportShop.Models
+{
+    public class EfProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public EfProductRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IQueryable<Product> Products => _dbContext.Products;
+    }
+}

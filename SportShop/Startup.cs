17c93afe7d62bcaea6ff4c8396bf4bde207f0afe
dominikwantuch <dominikwantuch@protonmail.com>
2019-10-11 12:01:47 +0200

    Add necessary usings.

--- SportShop/Startup.cs
+++ SportShop/Startup.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SportShop.Models;
 

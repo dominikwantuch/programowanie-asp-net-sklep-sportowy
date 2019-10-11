327e2628f9a00ca096f36aec00b5276888433ffe
dominikwantuch <dominikwantuch@protonmail.com>
2019-10-11 12:01:38 +0200

    Add configuration.

--- SportShop/Startup.cs
+++ SportShop/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,6 +12,13 @@ namespace SportShop
 {
     public class Startup
     {
+        public IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)

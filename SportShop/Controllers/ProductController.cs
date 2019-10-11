c277e04847adf5a5dd4fcadf014c7235ea8ec647
dominikwantuch <dominikwantuch@protonmail.com>
2019-10-11 10:43:12 +0200

    Update folder structure.

---
+++ SportShop/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportShop.Models;
+
+namespace SportShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        
+        public ViewResult List() => View(_productRepository.Products);
+    }
+}

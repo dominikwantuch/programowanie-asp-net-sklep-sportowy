636d1c01314ae7681513c84a6a5f744e03451108
dominikwantuch <dominikwantuch@protonmail.com>
2019-11-18 22:58:06 +0100

    Add IProductRepository.

---
+++ SportShop/Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using SportShop.Models;
+
+namespace SportShop.Repositories
+{
+    public interface IProductRepository
+    {
+        IQueryable<Product> Products { get; }
+        bool DeleteProduct(int id);
+        bool SaveProduct(Product entity);
+    }
+}

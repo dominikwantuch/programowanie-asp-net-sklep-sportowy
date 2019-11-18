80d840ef91e1fd6f62d9f32e093da77e7b4e01e3
dominikwantuch <dominikwantuch@protonmail.com>
2019-11-18 22:54:47 +0100

    Add main menu view component.

---
+++ SportShop/ViewComponents/MainMenuViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportShop.ViewComponents
+{
+    public class MainMenuViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}

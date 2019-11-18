2a9c1d6b2489e5dbe1c46995410ef7a76f6d417d
dominikwantuch <dominikwantuch@protonmail.com>
2019-11-18 23:03:36 +0100

    Add function that takes care of populating empty db.

--- SportShop/Startup.cs
+++ SportShop/Startup.cs
@@ -33,6 +33,7 @@ namespace SportShop
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            SeedData.EnsurePopulated(app);
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();

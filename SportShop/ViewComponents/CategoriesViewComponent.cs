faa2d4b7577129f26e39fa2133275c345a1aa811
dominikwantuch <dominikwantuch@protonmail.com>
2019-11-18 22:54:06 +0100

    Add Categories component.

---
+++ SportShop/ViewComponents/CategoriesViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportShop.Models;
+using SportShop.Repositories;
+
+namespace SportShop.ViewComponents
+{
+    public class CategoriesViewComponent : ViewComponent
+    {
+        private IProductRepository _repository;
+
+        public CategoriesViewComponent(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+        
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(_repository.Products.Select(x => x.Category).Distinct().ToList());
+        }
+    }
+}

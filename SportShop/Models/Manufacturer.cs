fbd7500c71179b9686c907d3f0b64c2a0dd5689e
dominikwantuch <dominikwantuch@protonmail.com>
2019-11-18 22:51:56 +0100

    Add Manufacturer model.

---
+++ SportShop/Models/Manufacturer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SportShop.Models
+{
+    public class Manufacturer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public ICollection<Product> Products { get; set; }
+
+    }
+}

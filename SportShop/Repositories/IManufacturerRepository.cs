aa9a4b1eeb37a276a24b95dd173ff86d045f78e9
dominikwantuch <dominikwantuch@protonmail.com>
2019-11-18 22:57:52 +0100

    Add IManufacturerRepository.

---
+++ SportShop/Repositories/IManufacturerRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using SportShop.Models;
+
+namespace SportShop.Repositories
+{
+    public interface IManufacturerRepository
+    {
+        IQueryable<Manufacturer> Manufacturers { get; }
+    }
+}

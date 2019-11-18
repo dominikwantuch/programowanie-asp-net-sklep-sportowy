df04ca690f560c78eeda1944ea6d76337b57ed99
dominikwantuch <dominikwantuch@protonmail.com>
2019-11-18 23:01:07 +0100

    Add ForeignKey attribute and proper spacing.

--- SportShop/Models/Product.cs
+++ SportShop/Models/Product.cs
@@ -1,11 +1,22 @@
-﻿namespace SportShop.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace SportShop.Models
 {
     public class Product
     {
         public int ProductId { get; set; }
+        
+        [ForeignKey("Manufacturer")]
+        public int? ManufacturerId { get; set; }
         public string Name { get; set; }
+        
         public string Description { get; set; }
+        
         public decimal Price { get; set; }
+        
         public string Category { get; set; }
+        
+        public Manufacturer Manufacturer { get; set; }
     }
 }

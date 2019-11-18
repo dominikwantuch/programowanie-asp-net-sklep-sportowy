f82ea20bcc7804347888e687a25051cc072f63bc
dominikwantuch <dominikwantuch@protonmail.com>
2019-11-18 22:52:06 +0100

    Add Result Model.

---
+++ SportShop/Models/ResultModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace SportShop.Models
+{
+    public class ResultModel<T>
+    {
+        public ResultModel(T data, int statusCode)
+        {
+            Data = data;
+            StatusCode = statusCode;
+        }
+        public T Data { get; set; }
+        
+        public int StatusCode { get; set; }
+    }
+}

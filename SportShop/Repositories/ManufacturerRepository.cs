ab0d243dfc4a34fd7038406067a55a1ef01d2e0a
dominikwantuch <dominikwantuch@protonmail.com>
2019-11-18 22:55:09 +0100

    Add Manufacturer repository.

---
+++ SportShop/Repositories/ManufacturerRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using SportShop.Models;
+
+namespace SportShop.Repositories
+{
+    public class ManufacturerRepository : IManufacturerRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ManufacturerRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IQueryable<Manufacturer> Manufacturers => _dbContext.Manufacturers.Include(x => x.Products);
+    }
+}

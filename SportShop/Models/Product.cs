56e5724f23fccce6f2c83cfd4541921f4ccebb08
dominikwantuch <dominikwantuch@protonmail.com>
2019-11-22 11:14:31 +0100

    Add model state checking attributes.

--- SportShop/Models/Product.cs
+++ SportShop/Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace SportShop.Models
@@ -9,12 +10,17 @@ namespace SportShop.Models
         
         [ForeignKey("Manufacturer")]
         public int? ManufacturerId { get; set; }
+        
+        [Required]
         public string Name { get; set; }
         
+        [Required]
         public string Description { get; set; }
         
+        [Required]
         public decimal Price { get; set; }
         
+        [Required]
         public string Category { get; set; }
         
         public Manufacturer Manufacturer { get; set; }

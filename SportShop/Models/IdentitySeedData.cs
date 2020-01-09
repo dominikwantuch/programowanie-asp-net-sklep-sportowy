089f6b21d741c54c1ad70d84dda38a24bcd08bab
dominikwantuch <dominikwantuch@protonmail.com>
2020-01-09 23:10:11 +0100

    Add user seeding.

---
+++ SportShop/Models/IdentitySeedData.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace SportShop.Models
+{
+    /// <summary>
+    /// Class for seeding users
+    /// </summary>
+    public static class IdentitySeedData
+    {
+        private const string adminUser = "Admin";
+        private const string adminPassword = "test123";
+
+        /// <summary>
+        /// Makes sure that database is populated.
+        /// </summary>
+        /// <param name="userManager"></param>
+        /// <returns></returns>
+        public static async Task EnsurePopulated(UserManager<IdentityUser> userManager)
+        {
+
+            IdentityUser user = await userManager.FindByIdAsync(adminUser);
+            if (user == null)
+            {
+                user = new IdentityUser(adminUser);
+                await userManager.CreateAsync(user, adminPassword);
+            }
+        }
+
+    }
+}

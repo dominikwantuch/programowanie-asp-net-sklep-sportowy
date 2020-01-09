ff8f02c01a83c0c45c06f29e5369c032f5e2496e
dominikwantuch <dominikwantuch@protonmail.com>
2020-01-09 23:09:48 +0100

    Add login viewmodel.

---
+++ SportShop/Models/Login.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportShop.Models
+{
+    public class Login
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; } = "/";
+    }
+}

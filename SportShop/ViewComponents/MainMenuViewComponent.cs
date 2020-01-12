6434c9d0908909f7b9f560dee2a822b0cc28b7c2
dominikwantuch <dominikwantuch@protonmail.com>
2020-01-12 01:14:03 +0100

    Add product list

--- SportShop/ViewComponents/MainMenuViewComponent.cs
+++ SportShop/ViewComponents/MainMenuViewComponent.cs
@@ -1,13 +1,21 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SportShop.Repositories;
 
 namespace SportShop.ViewComponents
 {
     public class MainMenuViewComponent : ViewComponent
     {
+        private readonly IProductRepository _productRepository;
+
+        public MainMenuViewComponent(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            return View(_productRepository.Products.Select(x=> x.Category).Distinct().ToList());
         }
     }
 }

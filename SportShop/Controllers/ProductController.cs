ca733ebb8cd42a1b35791c4803273a83933df4c5
dominikwantuch <dominikwantuch@protonmail.com>
2020-01-12 01:09:25 +0100

    Add proper comments.

--- SportShop/Controllers/ProductController.cs
+++ SportShop/Controllers/ProductController.cs
@@ -4,27 +4,38 @@ using SportShop.Repositories;
 
 namespace SportShop.Controllers
 {
-    [Route("products")]
+    /// <summary>
+    /// Controller of products view.
+    /// </summary>
+    [Route("product")]
     public class ProductController : Controller
     {
         private readonly IProductRepository _productRepository;
 
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="productRepository"></param>
         public ProductController(IProductRepository productRepository)
         {
             _productRepository = productRepository;
         }
+        
+        /// <summary>
+        /// Default application page. 
+        /// </summary>
         [HttpGet]
         [HttpGet("/")]
         public ViewResult Index(string name)
         {
             if (string.IsNullOrWhiteSpace(name))
             {
-                this.ViewBag.CurrentPage = "Main";
+                ViewBag.CurrentPage = "Main";
                 return View(_productRepository.Products);
             }
             else
             {
-                this.ViewBag.CurrentPage = "Categories";
+                ViewBag.CurrentPage = "Categories";
                 return View(_productRepository.Products.Where(x => x.Category == name));
             }
         }

bf7b3a1500426565a7d67e04c691c0adbc7c5756
dominikwantuch <dominikwantuch@protonmail.com>
2020-01-12 19:15:16 +0100

    Add ManufacturerModel

---
+++ SportShop/Models/ManufacturerModel.cs
@@ -0,0 +1,23 @@
+﻿using SportShop.Persistence.Entities;
+
+namespace SportShop.Models
+{
+    public class ManufacturerModel
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string Country { get; set; }
+
+        public static ManufacturerModel ToModel(Manufacturer entity)
+        {
+            return new ManufacturerModel
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                Country = entity.Country
+            };
+        }
+    }
+}

26facb2a63dad07b42ae1bb1df9ce184f42aab9d
dominikwantuch <dominikwantuch@protonmail.com>
2020-01-12 19:15:51 +0100

    Add ProductModel

---
+++ SportShop/Models/ProductModel.cs
@@ -0,0 +1,35 @@
+﻿using SportShop.Persistence.Entities;
+
+namespace SportShop.Models
+{
+    public class ProductModel
+    {
+        public int ProductId { get; set; }
+
+        public int? ManufacturerId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+        /// <summary>
+        /// Maps <see cref="entity"/> to <see cref="ProductModel"/>
+        /// </summary>
+        public static ProductModel ToModel(Product entity)
+        {
+            return new ProductModel
+            {
+                ProductId = entity.ProductId,
+                ManufacturerId = entity.ManufacturerId,
+                Name = entity.Name,
+                Description = entity.Description,
+                Price = entity.Price,
+                Category = entity.Category
+            };
+        }
+    }
+}

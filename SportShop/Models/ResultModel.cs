c5d8d10d73608ee60e174393dbaa7052b2f8262b
dominikwantuch <dominikwantuch@protonmail.com>
2020-01-12 19:17:22 +0100

    Add isStatusCodeSuccess method

--- SportShop/Models/ResultModel.cs
+++ SportShop/Models/ResultModel.cs
@@ -12,5 +12,13 @@ namespace SportShop.Models
         public T Data { get; set; }
         
         public int StatusCode { get; set; }
+
+        public bool isStatusCodeSuccess()
+        {
+            if (StatusCode >= 200 && StatusCode < 300)
+                return true;
+
+            return false;
+        }
     }
 }

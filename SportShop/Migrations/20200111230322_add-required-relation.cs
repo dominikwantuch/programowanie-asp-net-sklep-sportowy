9c1ceae0ea3b0a9d1a85f38f82724ba9f0b5b502
dominikwantuch <dominikwantuch@protonmail.com>
2020-01-12 01:10:34 +0100

    Change manufacturer-product relation to required.

---
+++ SportShop/Migrations/20200111230322_add-required-relation.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SportShop.Migrations
+{
+    public partial class addrequiredrelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Manufacturers_ManufacturerId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ManufacturerId",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Manufacturers_ManufacturerId",
+                table: "Products",
+                column: "ManufacturerId",
+                principalTable: "Manufacturers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Manufacturers_ManufacturerId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ManufacturerId",
+                table: "Products",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Manufacturers_ManufacturerId",
+                table: "Products",
+                column: "ManufacturerId",
+                principalTable: "Manufacturers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}

ed80413f391eb972001d884f97d5ae68d8560644
dominikwantuch <dominikwantuch@protonmail.com>
2020-01-12 01:13:27 +0100

    Add "required" attributes for proper validation

--- SportShop/Models/Manufacturer.cs
+++ SportShop/Models/Manufacturer.cs
@@ -7,9 +7,12 @@ namespace SportShop.Models
     public class Manufacturer
     {
         public int Id { get; set; }
+        
+        [Required]
         public string Name { get; set; }
+        
+        [Required]
         public string Country { get; set; }
         public ICollection<Product> Products { get; set; }
-
     }
 }

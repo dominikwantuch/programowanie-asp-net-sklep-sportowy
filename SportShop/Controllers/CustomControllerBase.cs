6f6a526c890772cfa73d3698923e08dc6e49000e
dominikwantuch <dominikwantuch@protonmail.com>
2020-01-12 19:04:59 +0100

    Create custom controller base for results customization.

---
+++ SportShop/Controllers/CustomControllerBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http.Results;
+using Microsoft.AspNetCore.Mvc;
+using SportShop.Models;
+
+namespace SportShop.Controllers
+{
+    /// <summary>
+    /// A custom controller base class for an MVC controller with view support.
+    /// </summary>
+    public class CustomControllerBase : Controller
+    {
+        /// <summary>
+        /// Represents internal server error.
+        /// </summary>
+        /// <param name="error">Custom error message.</param>
+        protected ObjectResult InternalServerError(string error = "An unexpected internal server error has occured.") => StatusCode(500, error);
+    }
+}

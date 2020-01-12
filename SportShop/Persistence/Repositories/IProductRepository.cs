ca95fe809d856256d095e18fa2d53c36dc9d1c08
dominikwantuch <dominikwantuch@protonmail.com>
2020-01-12 19:14:55 +0100

    Add new methods definitions

---
+++ SportShop/Persistence/Repositories/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using SportShop.Models;
+using SportShop.Persistence.Entities;
+
+namespace SportShop.Persistence.Repositories
+{
+    public interface IProductRepository
+    {
+        IQueryable<Product> Products { get; }
+        bool DeleteProduct(int id);
+        bool SaveProduct(Product entity);
+        ResultModel<Product> GetById(int id);
+        ResultModel<Product> Create(Product entity);
+        ResultModel<Product> Update(Product entity);
+        ResultModel<Product> Delete(int id);
+    }
+}

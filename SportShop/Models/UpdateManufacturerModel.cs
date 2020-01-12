82b1f34eb9904cd811bf7c83ae124057f6cd9cf9
dominikwantuch <dominikwantuch@protonmail.com>
2020-01-12 19:11:28 +0100

    Add UpdateManufacturerModel

---
+++ SportShop/Models/UpdateManufacturerModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SportShop.Persistence.Entities;
+
+namespace SportShop.Models
+{
+    public class UpdateManufacturerModel
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string Country { get; set; }
+
+        public Manufacturer ToEntity(Manufacturer entity)
+        {
+            if(entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            entity.Name = Name;
+            entity.Country = Country;
+
+            return entity;
+        }
+    }
+}

1366b56b5a6c90ac0ee8f4f4b88100033b8c52d5
dominikwantuch <dominikwantuch@protonmail.com>
2020-01-13 13:38:07 +0100

    Create basic mock up

--- DominikUnitTests/ProductApiControllerTests/ApplicationDbContextMockHelper.cs
+++ DominikUnitTests/ProductApiControllerTests/ApplicationDbContextMockHelper.cs
@@ -1,14 +1,65 @@
-﻿using Moq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using SportShop.Persistence.Entities;
 using SportShop.Persistence.Repositories;
 
 namespace DominikUnitTests.ProductApiControllerTests
 {
     public class ApplicationDbContextMockHelper
     {
-        public readonly Mock<ApplicationDbContext> Mock = new Mock<ApplicationDbContext>();
-
+        public readonly ApplicationDbContext MockedDbContext;
+        
+        List<Product> _products = new List<Product>()
+        {
+            new Product()
+            {
+                ProductId = 1,
+                ManufacturerId = 1,
+                Name = "Piłka plażowa",
+                Description = "Tania piłka do sportów plażowych",
+                Price = 55,
+                Category = "Sporty Wodne",
+            },
+            new Product()
+            {
+                ProductId = 2,
+                ManufacturerId = 1,
+                Name = "Płetwy dla nurka",
+                Description = "Płetwy w rozmiarze uniwersalnym.",
+                Price = 158,
+                Category = "Sporty Wodne",
+            },
+            new Product()
+            {
+                ProductId = 3,
+                ManufacturerId = 2,
+                Name = "Gra o tron",
+                Description = "Pionki, karty, mapy",
+                Price = 255,
+                Category = "Gry planszowe",
+            }
+        };
         public ApplicationDbContextMockHelper()
         {
+            MockedDbContext = new ApplicationDbContext(new DbContextOptions<ApplicationDbContext>());
+            var mockedDbSet = GetQueryableMockDbSet(_products);
+            MockedDbContext.Products = mockedDbSet;
+        }
+        
+        private static DbSet<T> GetQueryableMockDbSet<T>(List<T> sourceList) where T : class
+        {
+            var queryable = sourceList.AsQueryable();
+
+            var dbSet = new Mock<DbSet<T>>();
+            dbSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(queryable.Provider);
+            dbSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(queryable.Expression);
+            dbSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
+            dbSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(() => queryable.GetEnumerator());
+            dbSet.Setup(d => d.Add(It.IsAny<T>())).Callback<T>((s) => sourceList.Add(s));
+
+            return dbSet.Object;
         }
     }
 }

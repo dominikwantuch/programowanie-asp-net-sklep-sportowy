54dbaefebb6fe988bb3d3b3ea528e0e65bafc578
dominikwantuch <dominikwantuch@protonmail.com>
2020-01-13 12:31:54 +0100

    Add CreateProductModel unit tests

---
+++ DominikUnitTests/ProductApiControllerTests/CreateProductModelTests.cs
@@ -0,0 +1,31 @@
+﻿using SportShop.Models;
+using Xunit;
+
+namespace DominikUnitTests.ProductApiControllerTests
+{
+    public class CreateProductModelTests
+    {
+        private readonly CreateProductModel _model = new CreateProductModel()
+        {
+            ManufacturerId = 1,
+            Name = "ProductName",
+            Description = "ProductDescription",
+            Price = 100,
+            Category = "ProductCategory"
+        };
+
+        [Fact]
+        public void ShouldMapCreateProductModelToEntity()
+        {
+            var entity = _model.ToEntity();
+            
+            Assert.Equal(0, entity.ProductId);
+            Assert.Equal(_model.ManufacturerId, entity.ManufacturerId);
+            Assert.Equal(_model.Name, entity.Name);
+            Assert.Equal(_model.Description, entity.Description);
+            Assert.Equal(_model.Price, entity.Price);
+            Assert.Equal(_model.Category, entity.Category);
+
+        }
+    }
+}

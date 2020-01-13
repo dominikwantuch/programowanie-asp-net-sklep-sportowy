593ab84d7518183f8a8b4da44cb587ef12d98ede
dominikwantuch <dominikwantuch@protonmail.com>
2020-01-13 12:57:35 +0100

    Create ApplicationDbContext Mock Helper

---
+++ DominikUnitTests/ProductApiControllerTests/ApplicationDbContextMockHelper.cs
@@ -0,0 +1,14 @@
+﻿using Moq;
+using SportShop.Persistence.Repositories;
+
+namespace DominikUnitTests.ProductApiControllerTests
+{
+    public class ApplicationDbContextMockHelper
+    {
+        public readonly Mock<ApplicationDbContext> Mock = new Mock<ApplicationDbContext>();
+
+        public ApplicationDbContextMockHelper()
+        {
+        }
+    }
+}

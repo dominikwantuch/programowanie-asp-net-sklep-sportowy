751e36ec848e22a819606e8e51738c0c78fa8f15
dominikwantuch <dominikwantuch@protonmail.com>
2020-01-13 23:45:03 +0100

    Add TestBase because xUnit does not support global Setup/Teardown

---
+++ SeleniumTests/TestBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+// ReSharper disable CommentTypo
+
+namespace SeleniumTests
+{
+    public class TestBase : IDisposable
+    {
+        protected IWebDriver Driver;
+
+        protected TestBase()
+        {
+            Setup();
+
+        }
+        /// <summary>
+        /// Setup
+        /// </summary>
+        private void Setup()
+        {
+            // Needed to use GetCurrentDirectory() because selenium couldn't find geckodriver.exe by default
+            Driver = new FirefoxDriver(Directory.GetCurrentDirectory());
+        }
+        
+        /// <summary>
+        /// Teardown
+        /// </summary>
+        public void Dispose()
+        {
+            Driver.Close();
+        }
+    }
+}

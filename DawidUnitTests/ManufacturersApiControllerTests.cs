f7af76c84eb2f0600b8b94beb2abfba67d08550b
dburnat <d.burnat97@gmail.com>
2020-01-13 13:02:34 +0100

    Initial commit

---
+++ DawidUnitTests/ManufacturersApiControllerTests.cs
@@ -0,0 +1,12 @@
+﻿using Moq;
+using SportShop.Controllers;
+using SportShop.Persistence.Repositories;
+
+namespace UnitTests
+{
+    public class ManufacturersApiControllerTests
+    {
+        private readonly Mock<IManufacturerRepository> _mock = new Mock<IManufacturerRepository>();
+        private ManufacturersApiController _controller;
+    }
+}

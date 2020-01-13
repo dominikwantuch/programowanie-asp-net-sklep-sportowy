fe8b85e76bfde2f3c6efff95a52e4290f7f232f6
dburnat <d.burnat97@gmail.com>
2020-01-13 22:08:20 +0100

    Added delete and save tests for ManufacturerRepository

--- DawidUnitTests/ManufacturerRepositoryTests.cs
+++ DawidUnitTests/ManufacturerRepositoryTests.cs
@@ -15,7 +15,7 @@ namespace DawidUnitTests
     {
         private readonly Manufacturer _manufacturer = new Manufacturer
         {
-            Id = 1,
+            Id = 0,
             Name = "Razer",
             Country = "USA",
         };
@@ -53,17 +53,165 @@ namespace DawidUnitTests
                 context.AddRange(_manufacturers);
                 context.SaveChanges();
             }
-            
-            using(var context = new ApplicationDbContext(options))
-            using (var manufacturersRepository = new ManufacturerRepository(context))
+
+            using (var context = new ApplicationDbContext(options))
             {
-                var result = manufacturersRepository.Manufacturers;
-                
+                var manufacturersRepository = new Mock<ManufacturerRepository>(context);
+                var result = manufacturersRepository.Object.Manufacturers;
+
                 Assert.NotNull(result);
                 Assert.Equal(3, result.Count());
             }
-
         }
-        
+
+        [Fact]
+        public void DeleteManufacturer_EntityIsNull_ShouldReturnFalse()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("407579ED-65B4-4165-8ED9-662DE394D3EA").Options;
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.AddRange(_manufacturers);
+                context.SaveChanges();
+            }
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var manufacturersRepository = new Mock<ManufacturerRepository>(context);
+                var result = manufacturersRepository.Object.DeleteManufacturer(4);
+
+                Assert.False(result);
+                Assert.Equal(3, manufacturersRepository.Object.Manufacturers.Count());
+            }
+        }
+
+        [Fact]
+        public void DeleteManufacturer_EntityIsFound_ShouldReturnTrueAndRemoveOneEntry()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("19D1029C-8394-4D73-B8E2-14477D81BDAC").Options;
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.AddRange(_manufacturers);
+                context.SaveChanges();
+            }
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var manufacturersRepository = new Mock<ManufacturerRepository>(context);
+                var result = manufacturersRepository.Object.DeleteManufacturer(3);
+
+                Assert.True(result);
+                Assert.Equal(2, manufacturersRepository.Object.Manufacturers.Count());
+            }
+        }
+
+        [Fact]
+        public void SaveManufacturer_EntityIdIs0_ShouldReturnTrue()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("61DCFC11-0727-47D6-8FBA-E5FB567104B9").Options;
+            using (var context = new ApplicationDbContext(options)) ;
+
+            var manufacturer = new Manufacturer
+            {
+                Id = 0,
+                Name = "Razer",
+                Country = "USA",
+            };
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var manufacturersRepository = new Mock<ManufacturerRepository>(context);
+                var result = manufacturersRepository.Object.SaveManufacturer(manufacturer);
+
+                Assert.True(result);
+                Assert.Equal(1, manufacturersRepository.Object.Manufacturers.Count());
+            }
+        }
+
+        [Fact]
+        public void SaveManufacturer_EntityIdIsGreaterThan0_ShouldReturnTrue()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("74072F25-F283-4B11-AC92-AE27096F79A7").Options;
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.AddRange(_manufacturers);
+                context.SaveChanges();
+            }
+
+            var manufacturer = new Manufacturer
+            {
+                Id = 3,
+                Name = "Razer",
+                Country = "USA",
+            };
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var manufacturersRepository = new Mock<ManufacturerRepository>(context);
+                var result = manufacturersRepository.Object.SaveManufacturer(manufacturer);
+
+                Assert.True(result);
+                Assert.Equal(3, manufacturersRepository.Object.Manufacturers.Count());
+            }
+        }
+
+        [Fact]
+        public void SaveManufacturer_EntityIdIsGreaterThan0AndDoesNotExistInRepo_ShouldReturnFalse()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("E09F073F-DF5D-48C4-964A-517A56034250").Options;
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.AddRange(_manufacturers);
+                context.SaveChanges();
+            }
+
+            var manufacturer = new Manufacturer
+            {
+                Id = 5,
+                Name = "Razer",
+                Country = "USA",
+            };
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var manufacturersRepository = new Mock<ManufacturerRepository>(context);
+                var result = manufacturersRepository.Object.SaveManufacturer(manufacturer);
+
+                Assert.False(result);
+                Assert.Equal(3, manufacturersRepository.Object.Manufacturers.Count());
+            }
+        }
+
+        [Fact]
+        public void SaveManufacturer_EntityIdIsSmallerThan0_ShouldReturnFalse()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("61DCFC11-0727-47D6-8FBA-E5FB567104B9").Options;
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.AddRange(_manufacturers);
+                context.SaveChanges();
+            }
+
+            var manufacturer = new Manufacturer
+            {
+                Id = -1,
+                Name = "Razer",
+                Country = "USA",
+            };
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var manufacturersRepository = new Mock<ManufacturerRepository>(context);
+                var result = manufacturersRepository.Object.SaveManufacturer(manufacturer);
+
+                Assert.False(result);
+                Assert.Equal(3, manufacturersRepository.Object.Manufacturers.Count());
+            }
+        }
     }
 }

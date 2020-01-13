eb90a99626854136d71650d1c5a194ad78a33191
dominikwantuch <dominikwantuch@protonmail.com>
2020-01-13 15:10:58 +0100

    Add delete and save product unit tests

--- DominikUnitTests/ProductApiControllerTests/ProductRepositoryTests.cs
+++ DominikUnitTests/ProductApiControllerTests/ProductRepositoryTests.cs
@@ -15,6 +15,8 @@ namespace DominikUnitTests.ProductApiControllerTests
             _productRepository = new ProductRepository(_mockHelper.MockedDbContext);
         }
 
+        #region products
+        
         [Fact]
         public void ShouldReturnQueryableCollectionOfProducts()
         {
@@ -23,5 +25,77 @@ namespace DominikUnitTests.ProductApiControllerTests
             Assert.NotNull(result);
             Assert.Equal(3, result.Count());
         }
+        
+        #endregion
+        
+        #region DeleteProduct
+        
+        [Fact]
+        public void DeleteProductShouldReturnTrue()
+        {
+            var result = _productRepository.DeleteProduct(1);
+            Assert.True(result);
+            Assert.Equal(2, _productRepository.Products.Count());
+        }
+        
+        [Fact]
+        public void DeleteProductShouldReturnFalse()
+        {
+            var result = _productRepository.DeleteProduct(5);
+            Assert.False(result);
+            Assert.Equal(3, _productRepository.Products.Count());
+        }
+        
+        #endregion
+        
+        #region SaveProduct
+
+        [Fact]
+        public void SaveProductShouldAddProductAndReturnTrue()
+        {
+            var result = _productRepository.SaveProduct(_mockHelper.SaveProductEntity);
+            
+            Assert.True(result);
+            Assert.Equal(4, _productRepository.Products.Count());
+        }
+
+        [Fact]
+        public void SaveProductShouldUpdateExistingProduct()
+        {
+            var result = _productRepository.SaveProduct(_mockHelper.SaveProductUpdateEntity);
+            Assert.True(result);
+            Assert.Equal(3, _productRepository.Products.Count());
+
+            var shouldBeModified =
+                _productRepository.Products.FirstOrDefault(x =>
+                    x.ProductId == _mockHelper.SaveProductUpdateEntity.ProductId);
+            
+            Assert.NotNull(shouldBeModified);
+            Assert.Equal(_mockHelper.SaveProductUpdateEntity.ManufacturerId, shouldBeModified.ManufacturerId);
+            Assert.Equal(_mockHelper.SaveProductUpdateEntity.Name, shouldBeModified.Name);
+            Assert.Equal(_mockHelper.SaveProductUpdateEntity.Description, shouldBeModified.Description);
+            Assert.Equal(_mockHelper.SaveProductUpdateEntity.Price, shouldBeModified.Price);
+            Assert.Equal(_mockHelper.SaveProductUpdateEntity.Category, shouldBeModified.Category);
+        }
+
+        [Fact]
+        public void SaveProductShouldNotFindProductAndReturnFalse()
+        {
+            var result = _productRepository.SaveProduct(_mockHelper.SaveProductNotExistingEntity);
+            
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void SaveProductShouldShouldReturnFalse()
+        {
+            var result = _productRepository.SaveProduct(_mockHelper.SaveProductOutOfRangeIdEntity);
+            
+            Assert.False(result);
+        }
+        
+        #endregion
+        
+        
     }
 }

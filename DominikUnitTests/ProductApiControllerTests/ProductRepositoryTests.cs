adf09a67027d75e34b3b9cca8e17ee3ffaa99d80
dominikwantuch <dominikwantuch@protonmail.com>
2020-01-13 13:38:46 +0100

    Add setup and first test

--- DominikUnitTests/ProductApiControllerTests/ProductRepositoryTests.cs
+++ DominikUnitTests/ProductApiControllerTests/ProductRepositoryTests.cs
@@ -1,7 +1,27 @@
-﻿namespace DominikUnitTests.ProductApiControllerTests
+﻿using System.Linq;
+using SportShop.Persistence.Repositories;
+using Xunit;
+
+namespace DominikUnitTests.ProductApiControllerTests
 {
     public class ProductRepositoryTests
     {
-        
+        private readonly ApplicationDbContextMockHelper _mockHelper;
+        private readonly ProductRepository _productRepository;
+
+        public ProductRepositoryTests()
+        {
+            _mockHelper = new ApplicationDbContextMockHelper();
+            _productRepository = new ProductRepository(_mockHelper.MockedDbContext);
+        }
+
+        [Fact]
+        public void ShouldReturnQueryableCollectionOfProducts()
+        {
+            var result = _productRepository.Products;
+            
+            Assert.NotNull(result);
+            Assert.Equal(3, result.Count());
+        }
     }
 }

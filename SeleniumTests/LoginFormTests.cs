b607d0eda9010b1b23e724979eb678ac039af485
dominikwantuch <dominikwantuch@protonmail.com>
2020-01-14 12:17:32 +0100

    Add tests for login and password

--- SeleniumTests/LoginFormTests.cs
+++ SeleniumTests/LoginFormTests.cs
@@ -33,5 +33,50 @@ namespace SeleniumTests
             //
             // Assert.StartsWith("https://www.google.pl/search?", Driver.Url);
         }
+
+        [Fact]
+        public void ShouldShowLoginRequiredValidationError()
+        {
+            Driver.Navigate().GoToUrl(_baseUrl);
+            
+            var homePage = new HomePage(Driver);
+            homePage.NavigateToLogin();
+            
+            var loginPage = new LoginPage(Driver);
+            
+            loginPage.FillPasswordField("test123");
+            loginPage.ClickSubmitButton();
+
+            var nameRequiredElement = Driver.FindElement(By.CssSelector("span[data-valmsg-for='Name']"));
+            var passwordRequiredElement = Driver.FindElement(By.CssSelector("span[data-valmsg-for='Password']"));
+            
+            Assert.True(nameRequiredElement.Displayed);
+            Assert.Equal("The Name field is required.", nameRequiredElement.Text);
+            Assert.Equal(_baseUrl + "/account/login", Driver.Url);
+            Assert.False(passwordRequiredElement.Displayed);
+        }
+
+        [Fact]
+        public void ShouldShowPasswordRequiredValidationError()
+        {
+            Driver.Navigate().GoToUrl(_baseUrl);
+            
+            var homePage = new HomePage(Driver);
+            homePage.NavigateToLogin();
+            
+            var loginPage = new LoginPage(Driver);
+            
+            loginPage.FillLoginTextField("admin");
+            loginPage.ClickSubmitButton();
+
+            var nameRequiredElement = Driver.FindElement(By.CssSelector("span[data-valmsg-for='Name']"));
+            var passwordRequiredElement = Driver.FindElement(By.CssSelector("span[data-valmsg-for='Password']"));
+            
+            Assert.True(passwordRequiredElement.Displayed);
+            Assert.Equal("The Password field is required.", passwordRequiredElement.Text);
+            Assert.Equal(_baseUrl + "/account/login", Driver.Url);
+            Assert.False(nameRequiredElement.Displayed);            
+        }
+
     }
 }

5c9c05707e45b96618ebb92dcf9e831630593028
dburnat <d.burnat97@gmail.com>
2020-01-14 15:26:16 +0100

    Add first selenium test for Edit form

---
+++ DawidSeleniumTests/EditProductFormTests.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+using DawidSeleniumTests.PageModels;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using Xunit;
+
+namespace DawidSeleniumTests
+{
+    public class EditProductFormTests : TestsFixture
+    {
+        private readonly string _url;
+
+        public EditProductFormTests()
+        {
+            _url = "https://localhost:5001";
+        }
+
+        [Fact]
+        public void ClickFirstProductToEdit_ShouldReturnUrlWithId1()
+        {
+            Driver.Navigate().GoToUrl(_url);
+            var helper = new LoginHelper(Driver);
+            helper.LoginToSystem();
+            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(2));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("div[class=container-fluid]")));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("a button")));
+            
+            var firstEditButton =
+                Driver.FindElements(By.CssSelector("a button")).First();
+            firstEditButton.Click();
+            
+            Assert.Equal(_url + "/admin/products/edit?id=1", Driver.Url);
+        }
+
+
+    }
+}

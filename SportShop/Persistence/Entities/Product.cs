1ed37f77c1d801ebec0cec785f9031b7194e9e92
Szymon Domalik <szymon.domalik@gmail.com>
2020-01-14 14:42:08 +0100

    Added required attributes to Product

--- SportShop/Persistence/Entities/Product.cs
+++ SportShop/Persistence/Entities/Product.cs
@@ -8,19 +8,26 @@ namespace SportShop.Persistence.Entities
         public int ProductId { get; set; }
         
         [Required]
+        [Display(Name = "ManufacturerId")]
         [ForeignKey("Manufacturer")]
         public int? ManufacturerId { get; set; }
-        
+
+        [StringLength(60, MinimumLength = 3)]
         [Required]
         public string Name { get; set; }
-        
-        [Required]
+
+        [Required(ErrorMessage = "Description is required")]
+        [Display(Name = "Description")]
         public string Description { get; set; }
-        
+
         [Required]
+        [Display(Name = "Price")]
+        [DataType(DataType.Currency)]
+        [Range(0.1, double.MaxValue, ErrorMessage = "Price must be grater than 0")]
         public decimal Price { get; set; }
-        
-        [Required]
+
+        [Required(ErrorMessage = "Category must be given")]
+        [Display(Name = "Category")]
         public string Category { get; set; }
         
         public Manufacturer Manufacturer { get; set; }

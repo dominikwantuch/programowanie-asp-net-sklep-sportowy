d3519b9466d33b1ff5e2b54d235ddc4da4b8bd89
dominikwantuch <dominikwantuch@protonmail.com>
2020-01-14 10:52:39 +0100

    Add login page model

---
+++ SeleniumTests/LoginPage.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace SeleniumTests
+{
+    public class LoginPage
+    {
+        private IWebDriver _driver;
+        public LoginPage(IWebDriver driver)
+        {
+            _driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "Name")]
+        public IWebElement LoginTextField { get; set; }
+
+        [FindsBy(How = How.Id, Using = "Password")]
+        public IWebElement PasswordTextField { get; set; }
+
+        [FindsBy(How = How.Id, Using = "submit")]
+        public IWebElement SubmitButton { get; set; }
+
+        public void FillLoginTextField(string content)
+        {
+            LoginTextField.SendKeys(content);
+        }
+
+        public void FillPasswordField(string content)
+        {
+            PasswordTextField.SendKeys(content);
+        }
+
+        public void ClickSubmitButton()
+        {
+            SubmitButton.Click();
+        }
+    }
+}

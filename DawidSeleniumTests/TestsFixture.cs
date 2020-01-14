0958a373ed34cb905f4a56d062936d234e1e4c0f
dburnat <d.burnat97@gmail.com>
2020-01-14 15:24:55 +0100

    Setup for Selenium tests

---
+++ DawidSeleniumTests/TestsFixture.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace DawidSeleniumTests
+{
+    public abstract class TestsFixture : IDisposable
+    {
+        public IWebDriver Driver;
+        
+        public TestsFixture()
+        {
+            Setup();
+        }
+
+        private void Setup()
+        {
+            Driver = new ChromeDriver(Directory.GetCurrentDirectory());
+            Driver.Manage().Window.FullScreen();
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+        public void Dispose()
+        {
+            Driver.Quit();
+        }
+    }
+}

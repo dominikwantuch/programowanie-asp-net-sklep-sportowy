f3ba80cca7f6036cf6e41d74a7badbf3c42cf1e1
dburnat <d.burnat97@gmail.com>
2020-01-14 16:18:00 +0100

    Add new tests

--- DawidSeleniumTests/EditProductFormTests.cs
+++ DawidSeleniumTests/EditProductFormTests.cs
@@ -26,7 +26,7 @@ namespace DawidSeleniumTests
             var firstEditButton =
                 Driver.FindElements(By.CssSelector("a button")).First();
             firstEditButton.Click();
-            
+
             Assert.Equal(_url + "/admin/products/edit?id=1", Driver.Url);
         }
 
@@ -50,6 +50,76 @@ namespace DawidSeleniumTests
             var firstProductName =
                 Driver.FindElements(By.TagName("h4")).First();
             Assert.Contains(editName, firstProductName.Text);
+            Assert.Equal(_url + "/admin/products/save", Driver.Url);
+        }
+
+        [Fact]
+        public void EditProductDescription_DescriptionTest_ShouldReturnProductWithChangedDescription()
+        {
+            Driver.Navigate().GoToUrl(_url);
+            var helper = new LoginHelper(Driver);
+            helper.LoginToSystem();
+
+            var firstEditButton =
+                Driver.FindElements(By.CssSelector("a button")).First();
+            firstEditButton.Click();
+
+            var editName = "DescriptionTest";
+
+            var editPage = new EditProductPage(Driver);
+            editPage.FillDescriptionTextField(editName);
+            editPage.ClickSubmitButton();
+
+            var firstProductDescription =
+                Driver.FindElements(By.TagName("h6")).First();
+            Assert.Contains(editName, firstProductDescription.Text);
+            Assert.Equal(_url + "/admin/products/save", Driver.Url);
+        }
+
+        [Fact]
+        public void EditProductPrice_Price300_ShouldReturnProductWithChangedPrice()
+        {
+            Driver.Navigate().GoToUrl(_url);
+            var helper = new LoginHelper(Driver);
+            helper.LoginToSystem();
+
+            var firstEditButton =
+                Driver.FindElements(By.CssSelector("a button")).First();
+            firstEditButton.Click();
+
+            var editName = "300";
+
+            var editPage = new EditProductPage(Driver);
+            editPage.FillPriceTextField(editName);
+            editPage.ClickSubmitButton();
+
+            var firstProductPrice =
+                Driver.FindElements(By.TagName("h5")).First();
+            Assert.Contains(editName, firstProductPrice.Text);
+            Assert.Equal(_url + "/admin/products/save", Driver.Url);
+        }
+
+        [Fact]
+        public void EditProductPrice_PriceIsText_ShouldReturnSamePriceAsBefore()
+        {
+            Driver.Navigate().GoToUrl(_url);
+            var helper = new LoginHelper(Driver);
+            helper.LoginToSystem();
+
+            var firstEditButton =
+                Driver.FindElements(By.CssSelector("a button")).First();
+            firstEditButton.Click();
+            
+            var editName = "abcds";
+
+            var editPage = new EditProductPage(Driver);
+            var actualPrice = editPage.PriceTextField.Text;
+            editPage.FillPriceTextField(editName);
+            editPage.ClickSubmitButton();
+
+            var firstProductPrice =
+                Driver.FindElements(By.TagName("h5")).First();
+            Assert.Contains(actualPrice, firstProductPrice.Text);
             Assert.Equal(_url + "/admin/products/save", Driver.Url);
         }
     }
